e8e86b6ab48a04bcdeadba5662ca092f58adf7bb
Jesse Brown <jabrown85@gmail.com>
2017-03-15 22:18:00 -0500

    trying to fix android

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 
   async componentWillMount() {
     if (Platform.OS === 'android') {
-      await Exponent.Font.loadAsync({
+      await Expo.Font.loadAsync({
         'Roboto': require('native-base/Fonts/Roboto.ttf'),
         'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
       });
